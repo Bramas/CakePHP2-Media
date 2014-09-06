48c65fffa73b3b8c68d80f064a64ed4c6587b1db
Quentin Bramas <quentin.bramas@gmail.com>
2014-09-06 19:20:53 +0200

    fix StaticHelper for static content

--- View/Helper/StaticHelper.php
+++ View/Helper/StaticHelper.php
@@ -9,10 +9,8 @@ class StaticHelper extends HtmlHelper
 	public function assetUrl($path, $options = array()) {
 
 		$staticDomain = $this->staticDomain;
-			echo Configure::read('Admin.staticDomain');
 		if($staticDomain === null)
 		{
-			echo Configure::read('Admin.staticDomain');
 			$staticDomain = Configure::read('Admin.staticDomain');
 		}
 		if(!empty($staticDomain) 
